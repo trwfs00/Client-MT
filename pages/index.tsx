5641491e03d3e786d5c1eef407fb61b2d5fbb272
kamonnscx <127631239+kamonnscx@users.noreply.github.com>
2023-09-22 15:26:45 +0700

    login eiei

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,8 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className={inter.className}>
-      <MyNav />
+      <MyNav/>
+      
     </div>
   )
 }
