7cceddb7fc0b1af922a39ec868d316a4d4cab6d0
jimimoss <lowzaokmi248@gmail.com>
2023-09-23 17:44:59 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,53 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import React, { useEffect, useState } from 'react'
 import MyNav from '@/components/navigation'
 import MyLogin from '@/components/loginForm'
+import Myproduct from '@/components/productstore'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+type Props = {
+  datas: [Data]
+}
+
+type Data = {
+  _id: string
+  type: string,
+  productName: string,
+  productDesc: string,
+  thumbnail: number,
+  idSKU: [{
+    _id: string
+  }],
+  created_at: string,
+  updated_at: string,
+  deleted_at: string
+}
+
+export const getServerSideProps = async () => {
+  try {
+    let response = await fetch('http://localhost:8080/product/')
+    let datas = await response.json()
+
+    return {
+      props: { datas: JSON.parse(JSON.stringify(datas)) }
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      props: { datas: [] },
+    }
+  }
+}
+
+export default function Home(props: Props) {
+  const [product, setStudents] = useState<[Data]>(props.datas)
   return (
     <div className={inter.className}>
-      <MyNav />
-      <MyLogin/>
+      {/* <MyNav />
+      <MyLogin /> */}
+      <Myproduct datas={product}></Myproduct>
     </div>
   )
 }
