042c0a9bd1f5f3354f826a825ba83a4a4e7296a4
Pimnapa Sriboonreuang <126116881+nampimnapa@users.noreply.github.com>
2023-09-23 19:17:19 +0700

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import MyNav from '@/components/navigation'
 import Footer from '@/components/footer2'
+// import Card from '@/components/product-card'
 
 export default function Home() {
   return (
-    <div className='h-[200vh]'>
+    <div className='h-[100vh]'>
       <MyNav/>
+      {/* <Card/> */}
       <Footer/>
     </div>
   )
