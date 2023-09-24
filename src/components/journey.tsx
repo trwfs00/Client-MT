10d47d4f9ff1483cce75465761754c530f2a59c9
kamonnscx <127631239+kamonnscx@users.noreply.github.com>
2023-09-24 21:56:23 +0700

    Journey

--- src/components/journey.tsx
+++ src/components/journey.tsx
@@ -3,61 +3,42 @@ import Image from 'next/image';
 import Myjour from '@/images/journey.svg';
 function journey() {
     return (
-        <div className="bg-white py-24 sm:py-32 -z-10">
-            <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
-                <div className="hero-overlay bg-opacity-60"></div>
-                <div className="hero-content text-center text-neutral-content">
-                    <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+        <div className="mx-auto">
+            <div className="hero h-96 md:h-[40em] lg:h-[50em]" style={{
+                backgroundImage: `url(${Myjour.src})`,
+            }}>
+                <div className="hero-overlay bg-opacity-0"></div>
+                <div className="hero-content justify-end text-center md:text-left text-neutral-content w-full">
+                    <div className="max-w-md lg:max-w-lg">
+                        <h1 className="mb-5 text-5xl font-bold text-gray-900 w-[8em]">Your Journey, Our Passion</h1>
+                        <p className="mb-5 text-lg text-gray-700">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <div className="flex  mx-auto">
+                            <button className="bg-transparent  text-black font-semibold  py-2 px-4 border hover:bg-base-200 border-current rounded-full">
+                                Our Story
+                            </button>
+                            <button
+                                className="ml-4 bg-transparent  text-black font-semibold  py-2 px-4 border hover:bg-base-200 border-current rounded-full">
+                                Find Our Store
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-            {/* <div className="hero w-screen h-screen ">
-                <Image className="object-cover object-center  " src={Myjour} alt="" />
-                <div className=" "></div>
-                <div className="  ">
-                    <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Your Journey, <br />Our Passion</h1>
-                        <p className="mb-5"> Made with Love From Earth's Heart to Your Heart In Each Sparkling Gem, a Tale of Love and Legacy Unfolds to Your Heart.</p>
-                        <div className="flex  mx-auto">
-                            <button className="bg-transparent  text-black font-semibold  py-2 px-4 border border-current rounded-full">
-                                Our Story
-                            </button>
-                            <button
-                                className="ml-4 bg-transparent  text-black font-semibold  py-2 px-4 border border-current rounded-full">
-                                Find Our Store
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-            {/* <div className="w-screen h-screen text-back" >
+            <div className="grid grid-cols-3 gap-4  text-md bg-white">
+                <div className="ml-5 p-6 md:p-8 border-x bg-gradient-to-b from-[ #0F172A] to-[#BCBCBC]">
+
+                    <span className='text-xl font-bold'>Free Shipping with Zero Worries</span>
+                    <p className='pt-5 text-md '>We offer free shipping on all orders within Thailand. <br /> Returns are accepted on all unworn items within 30 days of purchase.</p>
                 
-                <Image className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center  " src={Myjour} alt="" />
-                <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-                   
-                    <div className=" lg:w-5/12 w-full">
-                        <h1 className="my-4 text-5xl font-bold leading-tight">
-                            Your Journey, <br />Our Passion
-                        </h1>
-                        <p className="text-2xl mb-8">
-                            Made with Love From Earth's Heart to Your Heart In Each Sparkling Gem, a Tale of Love and Legacy Unfolds to Your Heart
-                        </p>
-                        <div className="flex justify-center mx-auto">
-    
-                            <button className="bg-transparent  text-black font-semibold  py-2 px-4 border border-current rounded-full">
-                                Our Story
-                            </button>
-                            <button
-                                className="ml-4 bg-transparent  text-black font-semibold  py-2 px-4 border border-current rounded-full">
-                                Find Our Store
-                            </button>
-                        </div>
-                    </div>
                 </div>
-            </div > */}
+                <div className="ml-5 p-6 md:p-8 border- bg-gradient-to-b from-[ #0F172A] to-[#BCBCBC]"><span className='text-xl font-bold'>Flexible on Self-Pickup</span>
+                    <p className='pt-5 text-md'>Choose "Self-Pickup" at checkout and make the appointment <br /> when you'd like to pick up your order.</p></div>
+                <div className="ml-5 mr-5 p-6 md:p-8 border-x bg-gradient-to-b from-[ #0F172A] to-[#BCBCBC]"><span className='text-xl font-bold'>Book an Appointment</span>
+                    <p className='pt-5 text-md'>Schedule Your exclusive appointment today to experience <br /> the Brilliance of Diamonds, Guided by our expert advisors.</p>
+
+                </div>
+
+            </div>
         </div>
     )
 }
