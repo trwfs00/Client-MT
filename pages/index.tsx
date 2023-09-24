60ede374b8dc045ab9be74aee71874ac2594fa2b
kamonnscx <127631239+kamonnscx@users.noreply.github.com>
2023-09-24 14:28:33 +0700

    categoriesmaires

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import MyNav from '@/components/navigation'
 import Footer from '@/components/footer2'
+import Over from '@/components/categories'
 
 export default function Home() {
   return (
     <div className='h-[200vh]'>
       <MyNav/>
-      <Footer/>
+      <Over/>
+      
     </div>
   )
 }
