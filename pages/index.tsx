c32d06a99f989fc2e8061221ff3d82dbf92f2224
Pimnapa Sriboonreuang <126116881+nampimnapa@users.noreply.github.com>
2023-09-24 17:46:56 +0700

    card,ctu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
 import MyNav from '@/components/navigation'
 import Footer from '@/components/footer2'
+import Card from '@/components/product-card'
+import Contactus from "@/components/contactus";
 
 export default function Home() {
   return (
     <div className='h-[100vh]'>
       <MyNav/>
-      <Footer/>
+      {/* <Card/> */}
+      <Contactus/>
+      {/* <Footer/> */}
     </div>
   )
 }
