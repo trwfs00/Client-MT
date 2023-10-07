c3411f65df9fd3c21134590ee55d48bab6c514df
kamonnscx <127631239+kamonnscx@users.noreply.github.com>
2023-10-08 03:52:11 +0700

    eiei

--- src/components/proflie.tsx
+++ src/components/proflie.tsx
@@ -117,11 +117,7 @@ return (
                         <div className="flex items-center justify-center  mt-10  ">
                             <label htmlFor="dropzone-file" className="  flex flex-col items-center justify-center border-2 border-gray-100 rounded-full cursor-pointer bg-gray-50 hover:bg-gray-100 ">
                                 <div className="flex flex-col items-center justify-center ">
-
                                     <Image className=" rounded-xl m-auto  opacity-40  hover:opacity-70 " width={150} height={100} src={En} alt="Logo" />
-                                    {/* <PencilIcon class="h-6 w-6 absolute text-gray-500" /> */}
-                                    {/* <PencilSquareIcon class="h-6 w-6 absolute text-gray-500" /> */}
-                                    {/* <p className='absolute text-lg'>Edit</p> */}
                                     <CameraIcon className="h-8 w-8 absolute text-gray-500" />
                                 </div>
                                 <input id="dropzone-file" type="file" className="hidden" />
@@ -171,70 +167,13 @@ return (
                         </div>
                         
                         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                            <dt className="text-sm font-medium leading-6 text-gray-900">Relationship (optional)</dt>
-                            <div className="sm:col-span-1">
-                                <Listbox value={selected} onChange={setSelectedre}>
-                                    {({ open }) => (
-                                        <>
-                                            <div className="relative mt-2">
-                                                <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300 sm:text-sm sm:leading-6">
-                                                    <span className="flex items-center">
-                                                        <span className="ml-3 block truncate">{selectedre.name}</span>
-                                                    </span>
-                                                    <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                                                        <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                                    </span>
-                                                </Listbox.Button>
-                                                <Transition
-                                                    show={open}
-                                                    as={Fragment}
-                                                    leave="transition ease-in duration-100"
-                                                    leaveFrom="opacity-100"
-                                                    leaveTo="opacity-0">
-                                                    <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-gray-300 ring-opacity-5 focus:outline-none sm:text-sm">
-                                                        {relationship.map((relationship) => (
-                                                            <Listbox.Option
-                                                                key={relationship.id}
-                                                                className={({ active }) =>
-                                                                    classNames(
-                                                                        active ? 'bg-gray-700 text-white' : 'text-gray-900',
-                                                                        'relative cursor-default select-none py-2 pl-3 pr-9'
-                                                                    )
-                                                                }
-                                                                value={relationship}
-                                                            >
-                                                                {({ selected, active }) => (
-                                                                    <>
-                                                                        <div className="flex items-center">
-
-                                                                            <span
-                                                                                className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
-                                                                            >
-                                                                                {relationship.name}
-                                                                            </span>
-                                                                        </div>
-
-                                                                        {selected ? (
-                                                                            <span
-                                                                                className={classNames(
-                                                                                    active ? 'text-white' : 'text-gray-900',
-                                                                                    'absolute inset-y-0 right-0 flex items-center pr-4'
-                                                                                )}
-                                                                            >
-                                                                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                                                            </span>
-                                                                        ) : null}
-                                                                    </>
-                                                                )}
-                                                            </Listbox.Option>
-                                                        ))}
-                                                    </Listbox.Options>
-                                                </Transition>
-                                            </div>
-                                        </>
-                                    )}
-                                </Listbox>
-                            </div>
+                            <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 ">Relationship (optional)</label>
+                            <select id="countries" className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5  ">
+                                <option selected>Relationship </option>
+                                <option value="Single">Single</option>
+                                <option value="Married">Married</option>
+                                <option value="Divorce">Divorce</option>
+                            </select>
                         </div>
                         
                         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
